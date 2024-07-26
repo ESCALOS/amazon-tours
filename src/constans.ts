b40a47829c699800aa018fb534d1744aa25c4e2a
Nanoka <stornblood6969@gmail.com>
2024-07-25 20:21:53 -0500

    "Refactored components and constants: updated MainSlider, TourCardItem, TourCards, and constants; added new components and constants; and modified Layout and index pages."

--- src/constans.ts
+++ src/constans.ts
@@ -75,21 +75,139 @@ export const tourCardItems = [
   {
     id: 1,
     imgPath: "/src/images/slide/1.png",
-    title: "Pacaya Samira"
+    title: "Iquitos Relax",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Evenlet itaque labor.",
+    duration: "3 días",
+    members: "5 personas",
+    price: "desde $575"
   },
   {
     id: 2,
     imgPath: "/src/images/slide/2.png",
-    title: "Pacaya Samira"
+    title: "Inolvidable Río Amazonas",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Evenlet itaque labor.",
+    duration: "3 días",
+    members: "5 personas",
+    price: "desde $575"
   },
   {
     id: 3,
     imgPath: "/src/images/slide/3.png",
-    title: "Pacaya Samira"
+    title: "Iquitos de Aventura",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Evenlet itaque labor.",
+    duration: "5 días",
+    members: "5 personas",
+    price: "desde $575"
   },
   {
     id: 4,
     imgPath: "/src/images/slide/4.png",
-    title: "Pacaya Samira"
+    title: "Explorando el Amazonas",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Evenlet itaque labor.",
+    duration: "3 días",
+    members: "5 personas",
+    price: "desde $575"
+  },
+  {
+    id: 5,
+    imgPath: "/src/images/slide/5.png",
+    title: "Heliconia Lodge",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Evenlet itaque labor.",
+    duration: "4-5 días",
+    members: "5 personas",
+    price: "desde $575"
+  },
+  {
+    id: 6,
+    imgPath: "/src/images/slide/6.png",
+    title: "Pacaya Samira",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Evenlet itaque labor.",
+    duration: "2-8 días",
+    members: "5 personas",
+    price: "desde $575"
   }
+]
+
+export const socialMedias = [
+  {
+    id: 1,
+    icon: "facebook",
+    link: "https://www.facebook.com/"
+  },
+  {
+    id: 2,
+    icon: "twitter",
+    link: "https://twitter.com/"
+  },
+  {
+    id: 3,
+    icon: "instagram",
+    link: "https://www.instagram.com/"
+  },
+  {
+    id: 4,
+    icon: "google-plus",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 5,
+    icon: "linkedin",
+    link: "https://www.linkedin.com/"
+  }
+]
+
+export const relatedLinks = [
+  {
+    id: 1,
+    title: "Mincetur",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 2,
+    title: "¿Y tú que planes?",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 3,
+    title: "GORE Amazonas",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 4,
+    title: "Tripadvisor",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 5,
+    title: "Marca Perú",
+    link: "https://www.google.com/"
+  },
+]
+
+export const otherInterests = [
+  {
+    id: 1,
+    title: "Irapay Amazon",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 2,
+    title: "Heliconia Lodge",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 3,
+    title: "Cumaceba Lodge",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 4,
+    title: "Pacaya Samiria",
+    link: "https://www.google.com/"
+  },
+  {
+    id: 5,
+    title: "Tours Diarios",
+    link: "https://www.google.com/"
+  },
 ]
